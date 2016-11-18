ec17ba51725be41570b3f3643f5c359683ef887e
kkennethlee <kyung.kenneth.lee@gmail.com>
2016-11-18 07:59:24 +0000

    got basic plotting to work

--- saltonsea.py
+++ saltonsea.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as pl
 
 #ignore settings
 np.seterr(over='ignore')
@@ -65,7 +66,8 @@ for i in range(0, numYearsInt):
         julianDayRow.append(JD)
         
         
-        #vapor pressure equation
+        #Penman's Equation. 
+        #es, a function of temperature, is saturated vapor pressure in air at certain temperature
         es = 0.6108 * np.exp((17.27 * temperature[j]) / (237.3 + temperature[j]))
         
         #vapor pressure row
@@ -103,11 +105,12 @@ for i in range(0, numYearsInt):
 
 
 #print(evaporationMatrix)
-tEvaporationMatrix = np.transpose(evaporationMatrix) #(mm/year)
-tEvaporationMatrix /= 304.8 #(ft/year)
+tEvaporationMatrix = np.transpose(evaporationMatrix) #(mm/month)
 
-#print(tEvaporationMatrix)
+tEvaporationMatrix /= 304.8 #(ft/month)
 
+#sum the elements by column to get yearly evaporation
+tEvaporationMatrix = np.sum(tEvaporationMatrix, axis=0)
 
 #SCENARIO 1##########
 
@@ -122,7 +125,7 @@ salinity_i = 44000 #initial salinity level of lake (mg/L)
 #chosen salinity of the inflow 10343(mg/L)
 ##multiply salinity of inflow 10343(mg/L) to the water inflow rate 1.643614549e+12(L/yr) to find saltmassperyear
 
-saltMassPerYear= 1.1173e+18 #total amount of salt in inflow (mg/yr)
+saltMassPerYear= 1.73e+16 #total amount of salt in inflow (mg/yr)
 
 waterLevel_i = -235 #initial water level compared to sea level (ft)
 volume_i = 15.93 #initial lake volume (mi^3)
@@ -131,30 +134,44 @@ surfaceArea_i = 357.67 # initial surface area (mi^2)
 xAxisYear = []
 yAxisWaterLevel = []
 yAxisSalinity = []
+
+#yAxisWaterLevel.append(waterLevel_i)
+#yAxisSalinity.append(salinity_i)
+
 for i in range(0, numYearsInt):
     #create x-axis
     xAxisYear.append(i)
 
-    #current water level = initial water level - evaporation + inflow
-    waterLevel = waterLevel_i - evaporation + (inflow/surfaceArea_i) * 5280 #ft
+    #(ft)      =     (ft)     -         (ft)            + (mi^3) /  mi^3      *     (ft)  
+    waterLevel = waterLevel_i - tEvaporationMatrix[i] + ((inflow/surfaceArea_i) * 5280) #ft
 
-    #surface area = 7.91 * water level + 2224.83
-    surfaceArea = 7.91 * waterLevel + 2224.83 #mi^2
+    # find surface area at the new water level using the calculated trend equation:
+    # f(x) = 7.9104x + 222.48 at residual of 0.9763
+    # (mi^2)    = 7.9104 *     ft     + 2224.83
+    surfaceArea = 7.9104 * waterLevel + 2224.83
 
     surfaceArea_i = surfaceArea
 
-    yAxisWaterLevel.append(waterLevel)
 
-    volume = 0.34 * waterLevel * + 97.06 #mi^3
+    # find volume at the new water level using the calculated trend equation:
+    # f(x) = 0.3425 x + 97.062 at residual of 0.9638
+    # (mi^3) = 0.3425 * waterLevel + 97.062
+    volume = 0.3425 * waterLevel + 97.062
 
+    # update the next initial water level
     waterLevel_i = waterLevel
 
-    #mg/L + mg/yr * yr / mi^3
+    #mg/L    = mg * yr / mi^3
     salinity = salinity_i + ((saltMassPerYear * (i + 1))/volume) #mg/L
 
+    yAxisWaterLevel.append(waterLevel)
     yAxisSalinity.append(salinity)
 
 
+#print(xAxisYear)
+#print(yAxisWaterLevel[0])
 
-print(yAxisWaterLevel)
-print(yAxisSalinity)
+
+pl.plot(xAxisYear, yAxisWaterLevel)
+pl.show()
+#print(yAxisSalinity)
